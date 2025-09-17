a178ae573e4f795e484d8082bc5bd61ee6031877
Zsolt Harmath <harmathzs@gmail.com>
2025-09-17 14:17:27 +0200

    Create App layout

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default class App extends React.Component {
+  render() {
+    return <div>
+      <form>
+        <h1>Termék hozzáadása a kosárhoz</h1>
+        <label htmlFor="input_nev">Termék neve: </label>
+        <input type="text" id="input_nev" name="input_nev" placeholder="Név..." /><br />
+        <label htmlFor="input_ar">Ár: </label>
+        <input type="number" id="input_ar" name="input_ar" value={0} /><br />
+        <label htmlFor="input_db">Mennyiség: </label>
+        <input type="text" id="input_db" name="ininput_dbput_nev" placeholder="Mennyiség..." /><br />             
+        <input type="button" id="btn_submit" name="btn_submit" value="Hozzáadás a kosárhoz" /><br />
+      </form>
+      <hr />
+      <h3>A kosár tartalma</h3>
+      <ul></ul>
+    </div>
+  }
 }
-
-export default App
